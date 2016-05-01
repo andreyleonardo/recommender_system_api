97506b3c3323aaa08cc100c91bfa6a1d15eccc4f
Andrey Leonardo <andrey.leonardo@gmail.com>
2016-05-01 14:05:52 -0300

    [CHANGE] rollback to puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,15 +15,15 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true # Change to false when not using ActiveRecord
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -34,27 +34,27 @@ set :ssh_options,     forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/i
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w(config/database.yml)
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs,  %w(tmp/pids tmp/cache tmp/sockets)
 
-# namespace :puma do
-#   desc 'Create Directories for Puma Pids and Socket'
-#   task :make_dirs do
-#     on roles(:app) do
-#       execute "mkdir #{shared_path}/tmp/sockets -p"
-#       execute "mkdir #{shared_path}/tmp/pids -p"
-#     end
-#   end
-#   before :start, :make_dirs
-# end
-
-namespace :deploy do
-  desc 'Create Directories for Unicorn Pids and Socket'
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  # desc 'Create Directories for Unicorn Pids and Socket'
+  # task :make_dirs do
+  #   on roles(:app) do
+  #     execute "mkdir #{shared_path}/tmp/sockets -p"
+  #     execute "mkdir #{shared_path}/tmp/pids -p"
+  #   end
+  # end
 
   desc 'Make sure local git is in sync with remote.'
   task :check_revision do
@@ -74,18 +74,19 @@ namespace :deploy do
   #   end
   # end
 
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:reload'
-  end
+  # desc 'Restart application'
   # task :restart do
-  #   on roles(:app), in: :sequence, wait: 5 do
-  #     invoke 'puma:restart'
-  #   end
+  #   invoke 'unicorn:reload'
   # end
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
 
-  before :starting,     :make_dirs
-  after :starting,      :check_revision
+  # before :starting,     :make_dirs
+  before :starting,     :check_revision
+  # after :starting,      :check_revision
   # after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
